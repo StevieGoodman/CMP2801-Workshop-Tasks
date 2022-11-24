88f3f5e3e2d274bb236be234e949ec00216ed51b
Stevie <mail@steviegoodman.com>
2022-11-24 10:16:21 +0000

    Completed Task 5

--- Week 8/main.cpp
+++ Week 8/main.cpp
@@ -2,6 +2,7 @@ using namespace std;
 #include <iostream>
 #include "src/Task3.h"
 #include "src/Task4.h"
+#include "src/Task5.h"
 
 int main() {
     cout << "=== TASK 3 ===" << endl;
@@ -10,6 +11,10 @@ int main() {
     
     cout << "=== TASK 4 ===" << endl;
     Task4::run();
+    cout << endl;
+
+    cout << "=== TASK 5 ===" << endl;
+    Task5::run();
     cout << endl;
     
     return 0;
---
+++ Week 8/src/Task5.cpp
@@ -0,0 +1,21 @@
+using namespace std;
+#include <iostream>
+#include "Task5.h"
+
+void Task5::run() {
+    A a1 = A(2);
+    A a2 = A(3);
+    a1.print();
+    a2.print();
+    A a3 = a1++;
+    a3.print();
+}
+
+int Task5::A::getValue() { return this->value; }
+
+Task5::A Task5::A::operator++(int) {
+    cout << value << endl;
+    return Task5::A(++value);
+}
+
+void Task5::A::print() { cout << "My value is " << value << endl; }
