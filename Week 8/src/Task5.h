3092e4a095acc8b007b6a35ce1c3b002d2dbc1a0
Stevie <mail@steviegoodman.com>
2022-12-01 09:11:42 +0000

    Added comments for task 5

--- Week 8/src/Task5.h
+++ Week 8/src/Task5.h
@@ -6,13 +6,19 @@ using namespace std;
 
 class Task5 {
 public:
+    // Runs task 5 code
     void static run();
 
+    // Completely pointless container for integer value
     class A {
     public:
+        // Instantiates A object using an integer value
         A(int v): value(v) {}
+        // Getter for A.value
         int getValue();
+        // Prints out A.value
         void print();
+        // Increments A.value
         A operator++(int);
     private:
         int value;
